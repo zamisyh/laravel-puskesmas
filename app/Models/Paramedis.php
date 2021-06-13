535fd6a1d1e01b56568820486fca0edab5a5c273
dev-zam <zamsyh.work@gmail.com>
2021-06-13 21:30:26 +0700

    Update models paramedis and add relations

--- app/Models/Paramedis.php
+++ app/Models/Paramedis.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Paramedis extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama_paramedis', 'jenis_kelamin', 'no_izin_paramedis', 'tempat_lahir',
+        'tanggal_lahir', 'alamat', 'id_poli'
+    ];
+
+
+    public function poli()
+    {
+        return $this->belongsTo(Poli::class, 'id_poli', 'id');
+    }
 }
