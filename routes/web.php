0609e5554a067a8044b584332d8a2e9d7ea197f5
dev-zam <zamsyh.work@gmail.com>
2021-06-14 14:59:44 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Livewire\Admin\Components\Header;
 use App\Http\Livewire\Admin\ManagementUsers\Users;
 use App\Http\Livewire\Admin\MasterData\Paramedis;
 use App\Http\Livewire\Admin\MasterData\Poli;
+use App\Http\Livewire\Admin\MasterData\Jabatan;
+use App\Http\Livewire\Admin\MasterData\Bidang;
+use App\Http\Livewire\Admin\MasterData\Pegawai;
 
 use App\Http\Livewire\Admin\Home;
 
@@ -30,13 +33,13 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::prefix('dash')->group(function () {
-    Route::name('dash.')->group(function() {
+    Route::name('dash.')->group(function () {
         Route::middleware(['auth'])->group(function () {
             Route::get('/', Home::class)->name('home');
             Route::get('logout', Header::class)->name('logout');
 
             Route::middleware(['role:admin'])->group(function () {
-                Route::prefix('management')->group(function() {
+                Route::prefix('management')->group(function () {
                     Route::get('role', Roles::class)->name('management.role');
                     Route::get('users', Users::class)->name('management.user');
                 });
@@ -44,10 +47,11 @@ Route::prefix('dash')->group(function () {
                 Route::prefix('master-data')->group(function () {
                     Route::get('paramedis', Paramedis::class)->name('master-data.paramedis');
                     Route::get('poli', Poli::class)->name('master-data.poli');
+                    Route::get('jabatan', Jabatan::class)->name('master-data.jabatan');
+                    Route::get('bidang', Bidang::class)->name('master-data.bidang');
+                    Route::get('pegawai', Pegawai::class)->name('master-data.pegawai');
                 });
             });
-
-
         });
     });
 });
