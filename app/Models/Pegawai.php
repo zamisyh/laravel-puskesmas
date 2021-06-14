7a9bb191ea2a1de62d97ea80660b537f68aa1434
dev-zam <zamsyh.work@gmail.com>
2021-06-14 15:02:14 +0700

    Update models pegawai and add relations

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     use HasFactory;
+    protected $table = 'pegawai';
+
+    protected $fillable = [
+        'id_jabatan', 'id_bidang', 'nama_pegawai', 'jenis_kelamin',
+        'npwp', 'tempat_lahir', 'tanggal_lahir'
+    ];
+
+
+    public function bidang()
+    {
+        return $this->belongsTo(Bidang::class, 'id_bidang', 'id');
+    }
+
+    public function jabatan()
+    {
+        return $this->belongsTo(Jabatan::class, 'id_jabatan', 'id');
+    }
 }
