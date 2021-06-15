eb48269f1d2c88cd8a1104e0be2b786ff8855fb7
dev-zam <zamsyh.work@gmail.com>
2021-06-15 19:09:09 +0700

    Add relation id_poli on table dokters

--- database/migrations/2021_06_11_132821_create_jadwal_praktek_dokters_table.php
+++ database/migrations/2021_06_11_132821_create_jadwal_praktek_dokters_table.php
@@ -16,11 +16,13 @@ class CreateJadwalPraktekDoktersTable extends Migration
         Schema::create('jadwal_praktek_dokter', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_dokter');
+            $table->unsignedBigInteger('id_poli');
             $table->string('hari');
             $table->string('jam_mulai');
             $table->string('jam_selesai');
             $table->timestamps();
 
+            $table->foreign('id_poli')->references('id')->on('poli')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('id_dokter')->references('id')->on('dokter')->onDelete('cascade')->onUpdate('cascade');
         });
     }
