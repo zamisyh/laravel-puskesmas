59e73ebebbb63b4a00e9cd140cee316b8e51388f
dev-zam <zamsyh.work@gmail.com>
2021-06-16 20:25:51 +0700

    Update mode tindakan and add relations

--- app/Models/Tindakan.php
+++ app/Models/Tindakan.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Tindakan extends Model
 {
     use HasFactory;
+    protected $table = 'tindakan';
+
+    protected $fillable = [
+        'kode_tindakan', 'nama_tindakan', 'tindakan_oleh', 'id_poli'
+    ];
+
+    public function poli()
+    {
+        return $this->belongsTo(Poli::class, 'id_poli', 'id');
+    }
 }
