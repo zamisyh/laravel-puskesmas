9f003e5d6881015f94211275e24718be0a6e3199
dev-zam <zamsyh.work@gmail.com>
2021-06-16 20:26:28 +0700

    Update model operasi and add mass assignment

--- app/Models/Operasi.php
+++ app/Models/Operasi.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Operasi extends Model
 {
     use HasFactory;
+    protected $table = 'operasi';
+
+    protected $fillable = [
+        'kode_operasi', 'nama_operasi', 'biaya', 'tindakan_oleh'
+    ];
 }
