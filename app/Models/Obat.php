06a014b3f5ff6983f70eff70f21acc5625570a3c
dev-zam <zamsyh.work@gmail.com>
2021-06-16 20:26:43 +0700

    Update model obat and add mass assignment

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Obat extends Model
 {
     use HasFactory;
+    protected $table = 'obat';
+
+    protected $fillable = [
+        'kode_obat', 'nama_obat', 'jenis_obat', 'dosis_aturan_obat', 'satuan', 'sediaan'
+    ];
 }
