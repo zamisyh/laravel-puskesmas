ecd68004a93e5e9ecf316ab6b5da83ed09daf75d
dev-zam <zamsyh.work@gmail.com>
2021-06-18 20:28:12 +0700

    Update model stok obat and add relations

--- app/Models/StokObat.php
+++ app/Models/StokObat.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class StokObat extends Model
 {
     use HasFactory;
+    protected $table = 'stok_obat';
+
+    protected $fillable = [
+        'id_obat', 'jumlah'
+    ];
+
+    public function obat()
+    {
+        return $this->belongsTo(Obat::class, 'id_obat', 'id');
+    }
 }
