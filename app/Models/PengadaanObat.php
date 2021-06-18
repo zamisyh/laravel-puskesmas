27de3f1db19f3c5e482dfa47ef0ef3aed7dde9ed
dev-zam <zamsyh.work@gmail.com>
2021-06-18 20:28:25 +0700

    Update model pengadaan obat and add relations

--- app/Models/PengadaanObat.php
+++ app/Models/PengadaanObat.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class PengadaanObat extends Model
 {
     use HasFactory;
+    protected $table = 'pengadaan_obat';
+
+    protected $fillable = [
+        'no_trans', 'id_supplier', 'id_obat', 'harga_beli', 'jumlah', 'keterangan', 'total', 'tanggal_transaksi'
+    ];
+
+    public function obat()
+    {
+        return $this->belongsTo(Obat::class, 'id_obat', 'id');
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'id_supplier', 'id');
+    }
 }
