e68d28b5b0bd441205e9b36b6313a9e225a514b9
zam-dev <zamsyh.dev@gmail.com>
2021-11-12 14:19:53 +0700

    Update structure folder for deploy

---
+++ backend/resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.ico') }}"/>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+
+    @yield('css')
+
+    @livewireStyles
+</head>
+<body>
+    @yield('content')
+
+
+
+    @yield('js')
+
+
+
+    @livewireScripts
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <x-livewire-alert::scripts />
+</body>
+</html>
